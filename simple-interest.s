72b8645c6e1aba3596e1c71de42800790d085669
SammyDollaSign <hussam.mota@gmail.com>
2025-08-13 18:28:58 +0300

    Update simple-interest.s

--- simple-interest.s
+++ simple-interest.s
@@ -1,1 +1,8 @@
+read -p "Enter Principal amount: " principal
+read -p "Enter Rate of interest (in %): " rate
+read -p "Enter Time period (in years): " time
+
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "Simple Interest is: $interest"
 
